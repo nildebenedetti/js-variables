10b5976001785270567a544243c8c677900375f2
“Nilde <nilde.benedetti@gmail.com>
2026-03-20 14:56:58 +0100

    folder 01 - es 5 - solved

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const numOne = 8; // NON MODIFICARE QUESTA RIGA
 const numTwo = 16; // NON MODIFICARE QUESTA RIGA
 const numThree = 32; // NON MODIFICARE QUESTA RIGA
@@ -22,7 +24,7 @@ const numThreeMinusNumOne = numThree - numOne;
 console.log(numThreeMinusNumOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = NaN;
+const sum = Number(numOne) + Number(numTwo) + Number(numThree)
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
